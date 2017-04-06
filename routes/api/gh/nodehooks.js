354fd5ef615c3befa223fbbe6c1127b54f11d730
Adam Dunigan <adunigan@accruent.com>
2017-04-06 14:16:32 -0500

    accept post

--- routes/api/gh/nodehooks.js
+++ routes/api/gh/nodehooks.js
@@ -8,14 +8,12 @@ var client = new net.Socket();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
     var payload = req.body;
-    if(payload.hook.events.indexOf("push") > -1) {
-        console.log("Event received: Push to NodeHooks");
-        client.connect(6006, '127.0.0.1', function(){
-            client.write("git restart");
-            client.destroy();
-        });
-        res.sendStatus(200);
-    }
+    console.log("Event received: Push to NodeHooks");
+    client.connect(6006, '127.0.0.1', function(){
+        client.write("git restart");
+        client.destroy();
+    });
+    res.sendStatus(200);
 });
 
 module.exports = router;
