e70e1d501710982bf3422b613c127cecfe1e73a9
Adam Dunigan <adunigan@accruent.com>
2017-04-06 12:52:48 -0500

    updates

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var net = require('net');
 
+
+
+var client = new net.Socket();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource')
+    console.log("Testing console");
+    client.connect(6006, '127.0.0.1', function(){
+        client.write("Hello server");
+        client.destroy();
+    });
+    res.sendStatus(200);
 });
 
 module.exports = router;
