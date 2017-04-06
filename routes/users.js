94d618722f37fc6b3c4fcd1a9b8f4db7cf23efb5
Adam Dunigan <adunigan@accruent.com>
2017-04-06 12:55:47 -0500

    test

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   //res.send('respond with a resource')
     console.log("Testing console");
     client.connect(6006, '127.0.0.1', function(){
-        client.write("Hello server");
+        client.write("git restart");
         client.destroy();
     });
     res.sendStatus(200);
